8b7105c2c4d894f6376bd7552b8f04d74fa03197
Hexio <hexblacker@gmail.com>
2026-08-08 09:26:17 +0100

    Fix nginx proxy_pass trailing slash causing 502

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,12 +4,6 @@ set -e
 # Default BACKEND_URL if not set
 BACKEND_URL="${BACKEND_URL:-http://localhost:4000}"
 
-# Ensure trailing slash for proxy_pass
-case "$BACKEND_URL" in
-  */) ;;
-  *) BACKEND_URL="${BACKEND_URL}/" ;;
-esac
-
 export BACKEND_URL
 
 # Substitute env vars in nginx config template
